8b5dec5273fd868f55348a81a33810cfd254f329
CyberWordPL <cyberwordixx@gazeta.pl>
2024-09-08 14:39:39 +0200

    Add internal functions

--- libcolorify.c
+++ libcolorify.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <libcolorify.h>
 
-void set_default_color(libcolorify_ground_t grn)
+void __file_set_default_color(FILE *f, libcolorify_ground_t grn)
 {
     const int func_code = 39;
-    fprintf(stdout, "\x1b[%dm", func_code + (int)grn);
+    fprintf(f, "\x1b[%dm", func_code + (int)grn);
+}
+
+void __file_set_color_4b(FILE *f, libcolorify_color_4b_t col, libcolorify_ground_t grn)
+{
+    fprintf(f, "\x1b[%dm", (int)col + (int)grn);
+}
+
+void __file_set_color_8b(FILE *f, unsigned char col, libcolorify_ground_t grn)
+{
+    const int func_code = 38;
+    fprintf(f, "\x1b[%d;5;%um", func_code + (int)grn, col);
+}
+
+void __file_set_color_24b(FILE *f, unsigned char r, unsigned char g, unsigned char b, libcolorify_ground_t grn)
+{
+    const int func_code = 38;
+    fprintf(f, "\x1b[%u;2;%u;%u;%um", func_code + grn, r, g, b);
+}
+
+void set_default_color(libcolorify_ground_t grn)
+{
+    __file_set_default_color(stdout, grn);
 }
 
 void set_color_4b(libcolorify_color_4b_t col, libcolorify_ground_t grn)
 {
-    fprintf(stdout, "\x1b[%dm", (int)col + (int)grn);
+    __file_set_color_4b(stdout, col, grn);
 }
 
 void set_color_8b(unsigned char col, libcolorify_ground_t grn)
 {
-    const int func_code = 38;
-    fprintf(stdout, "\x1b[%d;5;%um", func_code + (int)grn, col);
+    __file_set_color_8b(stdout, col, grn); 
 }
 
 void set_color_24b(unsigned char r, unsigned char g, unsigned char b, libcolorify_ground_t grn)
 {
-    (void)grn;
-    const int func_code = 38;
-    fprintf(stdout, "\x1b[%u;2;%u;%u;%um", func_code + grn, r, g, b);
-    //fprintf(stdout, "%u %u %u", r, g, b);
+    __file_set_color_24b(stdout, r, g, b, grn);    
 }
--- libcolorify.h
+++ libcolorify.h
@@ -1,6 +1,8 @@
 #ifndef LIBCOLORIFY_HEADER
 #define LIBCOLORIFY_HEADER
 
+#include <stdio.h>
+
 typedef enum
 {
     LIBCOLORIFY_BLACK = 30,
@@ -27,6 +29,14 @@ typedef enum
     LIBCOLORIFY_FOREGROUND_COLOR,
     LIBCOLORIFY_BACKGROUND_COLOR = 10,
 } libcolorify_ground_t;
+
+#ifdef __LIBCOLORIFY_INTERNAL
+
+void __file_set_default_color(FILE *f, libcolorify_ground_t grn);
+void __file_set_color_4b(FILE *f, libcolorify_color_4b_t col, libcolorify_ground_t grn);
+void __file_set_color_8b(FILE *f, unsigned char col, libcolorify_ground_t grn);
+void __file_set_color_24b(FILE *f, unsigned char r, unsigned char g, unsigned char b, libcolorify_ground_t grn);
+#endif
 
 void set_default_color(libcolorify_ground_t grn);
 void set_color_4b(libcolorify_color_4b_t col, libcolorify_ground_t grn);
